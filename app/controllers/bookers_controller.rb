4cab018075b1e120630b0c56dd72c84d9be2b1bf
river-spring <rivermspring@gmail.com>
2020-06-20 18:57:22 +0900

    [Update]Bookers

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -3,19 +3,41 @@ class BookersController < ApplicationController
   end
 
   def index
-  	@books = Book.all
+  	@books = Book.all.order(created_at: :desc)
   	@book = Book.new
   end
 def create
-	book = Book.new(book_params)
-	book.save
-	redirect_to '/index'
+	@books = Book.all.order(created_at: :desc)
+	@book = Book.new(book_params)
+	if @book.save
+		flash[:notice] = "Book was successfully created."
+	   redirect_to booker_path(@book.id)
+	else
+		render("bookers/index")
+	end
 end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+  def update
+  	@book = Book.find(params[:id])
+  	 if @book.update(book_params)
+  	 	flash[:notice] = "Book was successfully updated."
+  	    redirect_to booker_path(@book.id)
+  	else
+  		render("bookers/edit")
+  	end
+  end
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	flash[:notice] = "Book was successfully destroyed."
+  	redirect_to bookers_path
   end
   private
 def book_params
