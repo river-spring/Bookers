389408c2b9f63101a73379f5e3d60197e1162d80
river-spring <rivermspring@gmail.com>
2020-06-20 13:36:36 +0900

    [upate]Bookers

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -3,11 +3,23 @@ class BookersController < ApplicationController
   end
 
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
+def create
+	book = Book.new(book_params)
+	book.save
+	redirect_to '/index'
+end
 
   def show
   end
 
   def edit
   end
+  private
+def book_params
+	params.require(:book).permit(:title, :body)
 end
+end
+
